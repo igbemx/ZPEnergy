9167fa511f52e551d973bb3d0d48627bff8101ab
Igor Beinik <igor.beinik@maxiv.lu.se>
2022-06-17 11:04:20 +0200

    removed socket from required packages in setup.py

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setuptools.setup(
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.6",
-    install_requires=['pytango', 'socket', 'scipy'],
+    install_requires=['pytango', 'scipy'],
     entry_points={
         'console_scripts': [
             'SoftiZPEnergy = ZPEnergy.ZPEnergy:main',
