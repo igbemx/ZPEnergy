07205cb4b8ce82bd4253993d65d1da22d3d16984
Igor Beinik <igor.beinik@maxiv.lu.se>
2022-07-25 13:45:51 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="ZPEnergy",
-    version="1.0.1",
+    version="1.0.2",
     description="Device server for ZPEnergy motor at Softimax",
     long_description=long_description,
     long_description_content_type="text/markdown",
