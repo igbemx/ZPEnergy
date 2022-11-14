2bcad773844a1ad675816753f75494fb09332298
Igor Beinik <igor.beinik@maxiv.lu.se>
2022-11-14 11:46:54 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,16 @@
-#!/usr/bin/env python3 
+#!/usr/bin/env python
 
 from setuptools import setup, find_packages
+
 
 setup(
     name="tangods-softimax-zpenergy",
     use_scm_version=True,
     setup_requires=["setuptools_scm"],
     description="Device server for ZPEnergy motor at Softimax",
-    author="Igor Beinik",
-    author_email="igor.beinik@maxiv.lu.se",
-    license="GPLv3",
     url="https://gitlab.maxiv.lu.se/softimax/tangods-softimax-zpenergy",
     packages=find_packages(exclude=["tests", "*.tests.*", "tests.*", "scripts"]),
-    python_requires=">=3.6",
     install_requires=['pytango', 'scipy'],
-    entry_points={
-        'console_scripts': [
-            'SoftiZPEnergy = SoftiZPEnergy.SoftiZPEnergy:main',
-        ],
-    },
+    include_package_data=True,
+    entry_points={"console_scripts": ["SoftiZPEnergy = SoftiZPEnergy.SoftiZPEnergy:main", ]},
 )
